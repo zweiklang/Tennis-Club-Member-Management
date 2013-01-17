243eab178f15b727b1991c4adf2ff27720abe5de
Marc Wegmann <marc.wegmann@gmail.com>
2013-01-17 13:43:06 +0100

    Bootstrap vollständig integriert, Simple Form integriert

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,7 @@
 class Member < ActiveRecord::Base
   attr_accessible :birthday, :email, :name, :phone, :status_id, :street, :zip, :first_name, :city
+
+  validates :email, :uniqueness => true
+  validates :name, :presence => true, :length => {:maximum => 60}
+  validates :first_name, :presence => true, :length => {:maximum => 60}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Members::Application.routes.draw do
   resources :members
 
-
   get "home/index"
 
   # The priority is based upon order of creation:
@@ -53,7 +52,7 @@ Members::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-root :to => 'home#index'
+root :to => 'members#index'
 
   # See how all your routes lay out with "rake routes"
 
