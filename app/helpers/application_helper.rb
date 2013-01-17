ce87d256fb375ca19d7ce3ea8ed10e5a36b7773d
Marc Wegmann <marc.wegmann@gmail.com>
2013-01-17 14:48:28 +0100

    added age calculation helper and age to view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # @param [Datetime] birthday
+  def age(birthday)
+    now = Time.now.utc.to_date
+    now.year - birthday.year - (birthday.to_date.change(:year => now.year) > now ? 1 : 0)
+  end
 end
